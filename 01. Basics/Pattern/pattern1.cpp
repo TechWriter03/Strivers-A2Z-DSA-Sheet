0d670045eee20a95e4767c66f1807eb267f35623
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-06 20:29:54 +0530

    Create pattern1.cpp

---
+++ 01. Basics/Pattern/pattern1.cpp
@@ -0,0 +1,24 @@
+// Input: ‘N’ = 3
+  
+// Output: 
+// * * *
+// * * *
+// * * *
+
+#include<bits/stdc++.h>
+using namespace std;
+void nForest(int n) 
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++) cout<<"* ";
+        cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    nForest(n);
+    return 0;
+}
