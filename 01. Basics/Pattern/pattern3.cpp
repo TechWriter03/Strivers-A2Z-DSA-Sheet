199c3a7b0278eb12907f15039187b27228de8975
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-06 21:05:29 +0530

    Update pattern3.cpp

--- 01. Basics/Pattern/pattern3.cpp
+++ 01. Basics/Pattern/pattern3.cpp
@@ -7,8 +7,9 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-void nTriangle(int n) {
-	for(int i=1;i<=n;i++)
+void nTriangle(int n) 
+{
+    for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=i;j++) cout<<j<<" ";
         cout<<"\n";
