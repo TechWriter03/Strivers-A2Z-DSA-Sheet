06e566da440c510f40637a8654724d21444ead78
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-06 21:11:53 +0530

    Create pattern6.cpp

---
+++ 01. Basics/Pattern/pattern6.cpp
@@ -0,0 +1,23 @@
+// Input: 
+// ‘N’ = 3
+// Output: 
+// 1 2 3
+// 1 2
+// 1
+
+#include<bits/stdc++.h>
+using namespace std;
+void nNumberTriangle(int n) {
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i+1;j++) cout<<j<<" ";
+        cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    nNumberTriangle(n);
+    return 0;
+}
