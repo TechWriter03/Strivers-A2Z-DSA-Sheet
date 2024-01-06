c727793256c98853730b526a5d746e6d6a2890c8
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-06 21:12:11 +0530

    Update pattern6.cpp

--- 01. Basics/Pattern/pattern6.cpp
+++ 01. Basics/Pattern/pattern6.cpp
@@ -7,7 +7,8 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-void nNumberTriangle(int n) {
+void nNumberTriangle(int n) 
+{
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=n-i+1;j++) cout<<j<<" ";
