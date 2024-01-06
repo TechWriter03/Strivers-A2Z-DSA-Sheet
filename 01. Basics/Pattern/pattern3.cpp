49008ee3971a4e6df3590b0a2faddae4bb3ee491
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-06 21:02:10 +0530

    Create pattern3.cpp

---
+++ 01. Basics/Pattern/pattern3.cpp
@@ -0,0 +1,23 @@
+// Input: 
+// ‘N’ = 3
+// Output: 
+// 1
+// 1 2 
+// 1 2 3
+
+#include<bits/stdc++.h>
+using namespace std;
+void nTriangle(int n) {
+	for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++) cout<<j<<" ";
+        cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    nTriangle(n);
+    return 0;
+}
