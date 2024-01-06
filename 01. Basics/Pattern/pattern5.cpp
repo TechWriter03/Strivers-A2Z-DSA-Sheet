f8374ba9ad3e4ab5b2a56682c678a165c2844aaa
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-06 21:08:17 +0530

    Create pattern5.cpp

---
+++ 01. Basics/Pattern/pattern5.cpp
@@ -0,0 +1,24 @@
+// Input: 
+// ‘N’ = 3
+// Output: 
+// * * *
+// * *
+// *
+
+#include<bits/stdc++.h>
+using namespace std;
+void seeding(int n) 
+{
+	  for(int i=n;i>=1;i--)
+    {
+        for(int j=i;j>=1;j--) cout<<"* ";
+        cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    seeding(n);
+    return 0;
+}
