eeec490e53ab43cc7f4ecf922e529684f52bb7aa
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-16 14:09:15 +0530

    Update pattern13.cpp

--- 01. Basics/Pattern/pattern13.cpp
+++ 01. Basics/Pattern/pattern13.cpp
@@ -1,6 +1,30 @@
-#include<bits/stdc++.h>
-int main()
+// Input: 
+// ‘N’ = 3
+// Output: 
+// 1
+// 2 3
+// 4 5 6
+
+#include <bits/stdc++.h>
+using namespace std;
+void nNumberTriangle(int n) 
 {
-    cout<<"pattern13 will be uploaded soon...";
+    int num=1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+}
+
+int main() 
+{
+    int n;
+    cin>>n;
+    nNumberTriangle(n);
     return 0;
 }
