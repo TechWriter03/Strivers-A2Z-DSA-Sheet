f36f3bbf299e2372634fc321d7ef67928b1a0825
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-16 14:13:29 +0530

    Update pattern15.cpp

--- 01. Basics/Pattern/pattern15.cpp
+++ 01. Basics/Pattern/pattern15.cpp
@@ -1,6 +1,30 @@
-#include<bits/stdc++.h>
-int main()
+// Input: 
+// ‘N’ = 3
+// Output: 
+// A B C
+// A B
+// A
+
+#include <bits/stdc++.h>
+using namespace std;
+void nLetterTriangle(int n) 
 {
-    cout<<"pattern15 will be uploaded soon...";
+    for(int i=n;i>0;i--)
+    {
+        char ch='A';
+        for(int j=1;j<=i;j++)
+        {
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+}
+
+int main() 
+{
+    int n;
+    cin>>n;
+    nLetterTriangle(n);
     return 0;
 }
