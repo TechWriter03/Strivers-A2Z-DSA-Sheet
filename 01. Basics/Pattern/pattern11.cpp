6938cf46167b4babb9ab1313e9f7173f587eea5b
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-16 14:03:05 +0530

    Update pattern11.cpp

--- 01. Basics/Pattern/pattern11.cpp
+++ 01. Basics/Pattern/pattern11.cpp
@@ -1,6 +1,32 @@
-#include<bits/stdc++.h>
-int main()
+// Input: 
+// ‘N’ = 3
+// Output: 
+// 1
+// 0 1
+// 1 0 1
+
+#include <bits/stdc++.h>
+using namespace std;
+void nBinaryTriangle(int n) 
 {
-    cout<<"pattern11 will be uploaded soon...";
+    int b;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==1) b=1;
+        else b=0;
+        for(int j=1;j<=i;j++)
+        {
+            cout<<b<<" ";
+            b=1-b;
+        }
+        cout<<endl;
+    }
+}
+
+int main() 
+{
+    int n;
+    cin>>n;
+    nBinaryTriangle(n);
     return 0;
 }
