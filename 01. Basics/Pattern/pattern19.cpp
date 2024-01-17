0314c5d6963c6772b65605e6515352f22d7d5202
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-17 21:21:28 +0530

    Create pattern19.cpp

---
+++ 01. Basics/Pattern/pattern19.cpp
@@ -0,0 +1,40 @@
+// Input: 
+// ‘N’ = 3
+// Output: 
+// * * * * * * 
+// * *     * * 
+// *         * 
+// *         * 
+// * *     * * 
+// * * * * * * 
+
+#include <bits/stdc++.h>
+using namespace std;
+void symmetry(int n) 
+{
+    int row=(2*n)-1,sp=0,star=n;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<star;j++) cout<<"* ";
+        for(int j=0;j<sp;j++) cout<<"  ";
+        for(int j=0;j<star;j++) cout<<"* ";
+        if(i<row/2)
+        {
+            star--;
+            sp+=2;
+        }
+        else
+        {
+            star++;
+            sp-=2;
+        }
+        cout<<endl;
+    }
+}
+int main() 
+{
+    int n;
+    cin>>n;
+    symmetry(n);
+    return 0;
+}
