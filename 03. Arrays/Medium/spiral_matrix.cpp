dfe39edb7434a18fb013932c99f8e87c05e51c2f
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-25 20:39:19 +0530

    Update spiral_matrix.cpp

--- 03. Arrays/Medium/spiral_matrix.cpp
+++ 03. Arrays/Medium/spiral_matrix.cpp
@@ -1,6 +1,32 @@
-#include<iostream.h>
-int main()
+// Input: 
+// matrix = 
+// [[1,2,3,4],
+//  [5,6,7,8],
+//  [9,10,11,12]]
+// Output: 
+// [1,2,3,4,8,12,11,10,9,5,6,7]
+
+vector<int> spiralOrder(vector<vector<int>>& matrix) 
 {
-    cout<<"will be uploaded soon...";
-    return 0;
+    int m=matrix.size(),n=matrix[0].size();
+    vector<int>ans;
+    int t=0,l=0,b=m-1,r=n-1;
+    while(t<=b && l<=r)
+    {
+        for(int i=l;i<=r;i++)
+            ans.push_back(matrix[t][i]);
+        t++;
+        for(int i=t;i<=b;i++)
+            ans.push_back(matrix[i][r]);
+        r--;
+        if(t<=b)
+            for(int i=r;i>=l;i--)
+                ans.push_back(matrix[b][i]);
+        b--;
+        if(l<=r)
+            for(int i=b;i>=t;i--)
+                ans.push_back(matrix[i][l]);
+        l++;
+    }
+    return ans;
 }
