a79242a68e6fb43a23beeb4498516d374f8006f5
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-01-27 20:52:35 +0530

    Update pascal_triangle.cpp

--- 03. Arrays/Hard/pascal_triangle.cpp
+++ 03. Arrays/Hard/pascal_triangle.cpp
@@ -1,7 +1,32 @@
-#include<iostream>
-using namespace std;
-int main()
+// Input: 
+// numRows = 5
+// Output: 
+// [[1],
+//  [1,1],
+//  [1,2,1],
+//  [1,3,3,1],
+//  [1,4,6,4,1]]
+
+vector<int> rows(int m) 
 {
-    cout<<"will be uploaded soon";
-    return 0;
+    vector<int>row;
+    int n=m,nr=1;
+    for(int i=0;i<n;i++)
+    {
+        if(i>0) 
+        {
+            nr*=m;
+            nr/=i;
+        }
+        row.push_back(nr);
+        m--;
+    }
+    return row;
+}
+vector<vector<int>> generate(int numRows) 
+{
+    vector<vector<int>>ans(numRows);
+    for(int i=0;i<numRows;i++)
+        ans[i]=rows(i+1);
+    return ans;
 }
