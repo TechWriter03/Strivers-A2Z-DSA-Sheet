fd7d40b1194b6fc2c353b709b58bd86232f900f0
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-03-03 21:29:27 +0530

    Create reverse_words.cpp

---
+++ 05. Strings/Basic/reverse_words.cpp
@@ -0,0 +1,45 @@
+// Input: 
+// s = "a good   example"
+// Output: 
+// "example good a"
+// Explanation: 
+// Reduce multiple spaces into single space and reverse the words.
+
+string reverseWords(string s) 
+{
+    int n=s.size();
+    string temp,ans;
+    vector<string>v;
+    while(s[0]==' ')
+    {
+        s.erase(0,1);
+        n--;
+    }
+    while(s[n-1]==' ')
+    {
+        s.erase(n-1,1);
+        n--;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(s[i]!=' ')
+        {
+            temp+=s[i];
+        }
+        else
+        {
+            if(temp!="") 
+                v.push_back(temp);
+            temp="";
+        }
+    }
+    v.push_back(temp);
+    int m=v.size();
+    for(int i=m-1;i>=0;i--)
+    {
+        ans+=v[i];
+        if(i>0)
+            ans+=" ";
+    }
+    return ans;
+}
