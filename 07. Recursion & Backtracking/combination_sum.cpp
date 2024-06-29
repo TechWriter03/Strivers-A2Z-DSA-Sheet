f3c4ef8577f7ea47376896787a10ee716dfe4589
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-06-29 23:34:53 +0530

    Update combination_sum.cpp

--- 07. Recursion & Backtracking/combination_sum.cpp
+++ 07. Recursion & Backtracking/combination_sum.cpp
@@ -4,7 +4,8 @@
 // Output: 
 // [[2,2,3],[7]]
 // Explanation:
-// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. 
+// Note that 2 can be used multiple times.
 // 7 is a candidate, and 7 = 7.
 
 void generate(int ind,int sum,int target,vector<int>& arr,vector<int>& ds,vector<vector<int>>& ans)
