67c224f00b92517b70900b9bdd1969c8bb399c45
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-07-15 13:11:55 +0530

    Create search_in_LL.cpp

---
+++ 06. Linked List/Singly Linked List/search_in_LL.cpp
@@ -0,0 +1,19 @@
+// Input:
+// 1->2->3->4
+// Key = 3
+// Output:
+// True
+// Explanation:
+// 3 is present in Linked List, so the function returns true.
+
+bool searchKey(int n, struct Node* head, int key) 
+{
+    Node* ptr=head;
+    while(ptr->next!=NULL)
+    {
+        if(ptr->data==key)
+            return true;
+        ptr=ptr->next;
+    }
+    return false;
+}
