a352f1ec81645b70975471d36566d34fdcb560cb
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2025-06-08 20:16:33 +0530

    Create level_order.cpp

---
+++ 13. Binary Trees/Traversals/level_order.cpp
@@ -0,0 +1,36 @@
+// Input:
+// root = [3,9,20,null,null,15,7]
+// Output:
+// [[3],[9,20],[15,7]]
+// Explanation:
+//   3
+//  / \
+// 9  20
+//   /  \
+//  15   7
+
+vector<vector<int>> levelOrder(TreeNode* root)
+{
+    vector<vector<int>>ans;
+    if(root==NULL)
+        return ans;
+    queue<TreeNode*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int size=q.size();
+        vector<int>level;
+        for(int i=0;i<size;i++)
+        {
+            TreeNode* node=q.front();
+            q.pop();
+            level.push_back(node->val);
+            if(node->left)
+                q.push(node->left);
+            if(node->right)
+                q.push(node->right);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
