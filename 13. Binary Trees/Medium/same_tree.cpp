d0e2a46ec214f379e8dac8d5f93977faa7830367
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2025-06-10 17:20:37 +0530

    Create same_tree.cpp

---
+++ 13. Binary Trees/Medium/same_tree.cpp
@@ -0,0 +1,22 @@
+// Input:
+// p = [1,2,3]
+// q = [1,2,3]
+// Output:
+// true
+// Explanation:
+//   1      1
+//  / \    / \
+// 2   3  2   3
+
+bool isSameTree(TreeNode* p, TreeNode* q)
+{
+    if(p==NULL || q==NULL)
+        return p==q;
+    if(p->val==q->val)
+    {
+        bool left=isSameTree(p->left,q->left);
+        bool right=isSameTree(p->right,q->right);
+        return left && right;
+    }
+    return false;
+}
