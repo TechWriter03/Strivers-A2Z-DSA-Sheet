37ca87204bd9e22a1e5119c11c8d51e77f6ef5b5
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2025-07-01 19:18:11 +0530

    Create iterator.cpp

---
+++ 14. Binary Search Trees/iterator.cpp
@@ -0,0 +1,50 @@
+// Input:
+// ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+// [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+// Output:
+// [null, 3, 7, true, 9, true, 15, true, 20, false]
+// Explanation:
+// BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+// bSTIterator.next();    // return 3
+// bSTIterator.next();    // return 7
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 9
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 15
+// bSTIterator.hasNext(); // return True
+// bSTIterator.next();    // return 20
+// bSTIterator.hasNext(); // return False
+
+class BSTIterator
+{
+public:
+    stack<TreeNode*>inorder;
+
+    BSTIterator(TreeNode* root)
+    {
+        pushAllLeft(root);
+    }
+
+    void pushAllLeft(TreeNode* node)
+    {
+        while(node->left)
+        {
+            inorder.push(node);
+            node=node->left;
+        }
+    }
+    
+    int next()
+    {
+        TreeNode* node=inorder.top();
+        inorder.pop();
+        if(node->right)
+            pushAllLeft(node->right);
+        return node->val;
+    }
+    
+    bool hasNext()
+    {
+        return inorder.empty();
+    }
+};
