3b0bc06c34014a1dfe3cb7ab2ec53241048c658f
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2026-03-08 11:48:33 +0530

    Update climbing_stairs.cpp

--- 16. DP/1D - DP/climbing_stairs.cpp
+++ 16. DP/1D - DP/climbing_stairs.cpp
@@ -9,45 +9,45 @@
 // 3. 2 steps + 1 step
 
 // Recursion
-int climbStairs(int n) 
+int climbStairs(int n)
 {
-    if(n==0) return 1;
-    if(n<0) return 0;
+    if(n==0 || n==1)
+        return 1;
     return climbStairs(n-1)+climbStairs(n-2);
 }
 
 // Memoization
 int findWays(int n,vector<int>& dp)
 {
-    if(n<0) return 0;
-    if(dp[n]!=-1) return dp[n];
+    if(n==0 || n==1)
+        return 1;
+    if(dp[n]!=-1)
+        return dp[n];
     return dp[n]=findWays(n-1,dp)+findWays(n-2,dp);
 }
-int climbStairs(int n) 
+int climbStairs(int n)
 {
-    vector<int>dp(n+1,-1);
-    dp[0]=1;
+    vector<int> dp(n+1,-1);
     return findWays(n,dp);
 }
 
 // Tabulation
-int climbStairs(int n) 
+int climbStairs(int n)
 {
-    vector<int>dp(n+1,-1);
-    dp[0]=1;
-    dp[1]=1;
+    vector<int> dp(n+1,-1);
+    dp[0]=1,dp[1]=1;
     for(int i=2;i<=n;i++)
         dp[i]=dp[i-1]+dp[i-2];
     return dp[n];
 }
 
 // Space Optimization
-int climbStairs(int n) 
+int climbStairs(int n)
 {
-    int prev1=1,prev2=1,curr;
+    int prev1=1,prev2=1;
     for(int i=2;i<=n;i++)
     {
-        curr=prev1+prev2;
+        int curr=prev1+prev2;
         prev2=prev1;
         prev1=curr;
     }
