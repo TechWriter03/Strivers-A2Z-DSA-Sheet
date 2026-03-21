5f703758d344cc2a3ed7f988991a24be637e6b7e
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2026-03-21 12:04:01 +0530

    Update min_path_sum_triangle.cpp

--- 16. DP/2D - DP/min_path_sum_triangle.cpp
+++ 16. DP/2D - DP/min_path_sum_triangle.cpp
@@ -11,62 +11,82 @@
 // The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11.
 
 // Recursion
-int minPath(vector<vector<int>>& arr,int n,int row,int col)
+int minPath(int row,int col,int n,vector<vector<int>>& triangle)
 {
-    if(row==n-1) 
-        return arr[row][col];
-    int down = minPath(arr,n,row+1,col);
-    int cross = minPath(arr,n,row+1,col+1);
-    return arr[row][col] + min(down,cross);
+    if(row==n-1)
+        return triangle[row][col];
+    int down=minPath(row+1,col,n,triangle);
+    int cross=minPath(row+1,col+1,n,triangle);
+    return triangle[row][col]+min(down,cross);
 }
-int minimumTotal(vector<vector<int>>& triangle) 
+
+int minimumTotal(vector<vector<int>>& triangle)
 {
     int n=triangle.size();
-    return minPath(triangle,n,0,0);
+    return minPath(0,0,n,triangle);
 }
 
 // Memoization
-int minPath(vector<vector<int>>& arr,int n,int row,int col,vector<vector<int>>& dp)
+int minPath(int row,int col,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp)
 {
-    if(row==n-1) 
-        return arr[row][col];
+    if(row==n-1)
+        return triangle[row][col];
     if(dp[row][col]!=-1)
         return dp[row][col];
-    int down = minPath(arr,n,row+1,col,dp);
-    int cross = minPath(arr,n,row+1,col+1,dp);
-    return dp[row][col] = arr[row][col] + min(down,cross);
+    int down=minPath(row+1,col,n,triangle,dp);
+    int cross=minPath(row+1,col+1,n,triangle,dp);
+    return dp[row][col]=triangle[row][col]+min(down,cross);
 }
-int minimumTotal(vector<vector<int>>& triangle) 
+
+int minimumTotal(vector<vector<int>>& triangle)
 {
     int n=triangle.size();
-    vector<vector<int>>dp(n,vector<int>(n,-1));
-    return minPath(triangle,n,0,0,dp);
+    vector<vector<int>> dp(n,vector<int>(n,-1));
+    return minPath(0,0,n,triangle,dp);
 }
 
 // Tabulation
-int minimumTotal(vector<vector<int>>& arr) 
+int minimumTotal(vector<vector<int>>& triangle)
 {
-    int n=arr.size();
-    vector<vector<int>>dp(n,vector<int>(n));
+    int n=triangle.size();
+    vector<vector<int>> dp(n,vector<int>(n,0));
     for(int i=0;i<n;i++)
-        dp[n-1][i] = arr[n-1][i];
+    {
+        dp[n-1][i]=triangle[n-1][i];
+    }
     for(int i=n-2;i>=0;i--)
+    {
         for(int j=i;j>=0;j--)
-            dp[i][j]=arr[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+        {
+            int down=dp[i+1][j];
+            int cross=dp[i+1][j+1];
+            dp[i][j]=triangle[i][j]+min(down,cross);
+        }
+    }
     return dp[0][0];
 }
 
 // Space Optimization
-int minimumTotal(vector<vector<int>>& arr) 
+int minimumTotal(vector<vector<int>>& triangle)
 {
-    int n=arr.size();
-    vector<int>curr(n),next(n);
-    for(int i=0;i<n;i++)
-        next[i] = arr[n-1][i];
-    for(int i=n-2;i>=0;i--)
+    int n=triangle.size();
+    vector<int> next(n,0);
+    for(int i=n-1;i>=0;i--)
     {
+        vector<int> curr(n,0);
         for(int j=i;j>=0;j--)
-            curr[j]=arr[i][j]+min(next[j],next[j+1]);
+        {
+            if(i==n-1)
+            {
+                curr[j]=triangle[n-1][j];
+            }
+            else
+            {
+                int down=next[j];
+                int cross=next[j+1];
+                curr[j]=triangle[i][j]+min(down,cross);
+            }
+        }
         next=curr;
     }
     return next[0];
